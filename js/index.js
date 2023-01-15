f2bef7e9ac078049f4555228f279cb74ffe37d14
junwanzhao <zhuhuiyu2499669698@163.com>
2023-01-16 03:34:10 +0800

    完成新上好课区域课程切换动态部分

--- js/index.js
+++ js/index.js
@@ -238,10 +238,32 @@
     clearInterval(timer);
   }
 
-  ul.onmouseleave= function () {
+  ul.onmouseleave = function () {
     timer = setInterval(() => {
       leftPx = --leftPx == -1920 ? 0 : leftPx;
       ul.style.left = leftPx + 'px';
     }, 10);
+  }
+}
+
+// 课程切换
+{
+  // 获取所有的 a 标签（tab 栏）
+  const tabs = document.querySelectorAll('.new-course-box a');
+  // 获取到所有的课程列表
+  const uls = document.querySelectorAll('.new-course-list ul');
+
+  for (let i = 0; i < tabs.length; i++) {
+    // 循环为所有的a绑定点击事件
+    tabs[i].onclick = function () {
+      // 清除所有的a和ul的样式
+      for (let j = 0; j < tabs.length; j++) {
+        tabs[j].className = '';
+        uls[j].className = '';
+      }
+      // 给当前选中的a和ul添加样式{
+      tabs[i].className = 'active';
+      uls[i].className = 'current';
+    }
   }
 }
